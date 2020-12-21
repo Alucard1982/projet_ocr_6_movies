02a57f2f2dba2aaf8e0d62588cccde62ac2b14cc
Florent <florentpeyre@hotmail.fr>
2020-12-21 14:50:10 +0100

    carousel ok

--- serveur.py
+++ serveur.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import requests
 
 app = Flask(__name__, static_url_path='/static')
@@ -15,12 +15,14 @@ def requeste():
     best_film_details = response.json()
 
     list_dic_best_movies = []
+    list_best_movies_json = []
     payload = {"imdb_score_min": "9.35",
                "page_size": "7"}
     response = requests.get('http://127.0.0.1:8000/api/v1/titles', params=payload)
     best_movies = response.json()
     for i in range((len(best_movies['results']))):
         response = requests.get((best_movies['results'][i]['url']))
+
         list_dic_best_movies.append(response.json())
     best_movies = list_dic_best_movies[:-3]
 
@@ -40,6 +42,33 @@ def requeste():
     return render_template("index.html", best_film=best_film_details,
                            categories_familly=categorie_familly,
                            all_best_movies=best_movies)
+
+
+@app.route("/best_movies/")
+def api_best_movies():
+    list_dic_best_movies = []
+    payload = {"imdb_score_min": "9.35",
+               "page_size": "7"}
+    response = requests.get('http://127.0.0.1:8000/api/v1/titles', params=payload)
+    best_movies = response.json()
+    for i in range((len(best_movies['results']))):
+        response = requests.get((best_movies['results'][i]['url']))
+        list_dic_best_movies.append(response.json())
+    return jsonify(list_dic_best_movies)
+
+
+@app.route("/familly/")
+def api_familly():
+    list_dic_familly = []
+    payload = {"genre": "Family",
+               "imdb_score_min": "8.9",
+               "page_size": "7"}
+    response = requests.get('http://127.0.0.1:8000/api/v1/titles', params=payload)
+    genre_film = response.json()
+    for i in range((len(genre_film['results']))):
+        response = requests.get((genre_film['results'][i]['url']))
+        list_dic_familly.append(response.json())
+    return jsonify(list_dic_familly)
 
 
 if __name__ == "__main__":
--- static/movies.js
+++ static/movies.js
@@ -2,12 +2,82 @@ var $dialog = document.getElementById('mydialog');
 var image = document.getElementById('film-notes');
 var list_image = document.getElementById('film-notes').getElementsByTagName('img');
 
+var carousel_best_movies_position1 = document.getElementById('film-notes').getElementsByClassName('slider_best_movies')[0]
+var carousel_best_movies_position2 = document.getElementById('film-notes').getElementsByClassName('slider_best_movies')[1]
+var carousel_best_movies_position3 = document.getElementById('film-notes').getElementsByClassName('slider_best_movies')[2]
+var carousel_best_movies_position4 = document.getElementById('film-notes').getElementsByClassName('slider_best_movies')[3];
+var click_forward_best_movies = document.getElementById('click_forward');
+var click_back_best_movies = document.getElementById('click_back');
 
-   for(var i = 0; i < list_image.length; i++){
-      console.log(list_image[i].src);
-   }
+var carousel_familly_position1 = document.getElementById('film-categorie1').getElementsByClassName('slider_familly')[0]
+var carousel_familly_position2 = document.getElementById('film-categorie1').getElementsByClassName('slider_familly')[1]
+var carousel_familly_position3 = document.getElementById('film-categorie1').getElementsByClassName('slider_familly')[2]
+var carousel_familly_position4 = document.getElementById('film-categorie1').getElementsByClassName('slider_familly')[3];
+var click_forward_familly= document.getElementById('click_forward_familly');
+var click_back_familly = document.getElementById('click_back_familly');
+
+var image_best_movies = [];
+var list_img_familly = [];
+var position = 0;
+
+function ajax_best_movies(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          data_list=JSON.parse(this.responseText);
+          for(var i = 0; i < data_list.length; i++){
+            image_best_movies.push(data_list[i]['image_url'])
+          }
+      }
+    };
+    xmlhttp.open("GET","http://127.0.0.1:5000/best_movies/", true);
+    xmlhttp.send();
+}
+function ajax_categorie_familly(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          data_list=JSON.parse(this.responseText);
+          for(var i = 0; i < data_list.length; i++){
+            list_img_familly.push(data_list[i]['image_url'])
+          }
+      }
+    };
+
+    xmlhttp.open("GET","http://127.0.0.1:5000/familly/", true);
+    xmlhttp.send();
+}
+function forwardClick(list, carousel1, carousel2, carousel3, carousel4){
+    position++;
+    carousel1.src=list[position%7];
+    carousel2.src=list[(position+1)%7];
+    carousel3.src=list[(position+2)%7];
+    carousel4.src=list[(position+3)%7];
+}
+function backClick(list, carousel1, carousel2, carousel3, carousel4){
+    if (position == 0){
+        position=list.length;
+    }
+    position--;
+    carousel1.src=list[position%7];
+    carousel2.src=list[(position+1)%7];
+    carousel3.src=list[(position+2)%7];
+    carousel4.src=list[(position+3)%7];
+}
+
+image.addEventListener('click', function(){ $dialog.showModal();});
+
+click_forward_best_movies.addEventListener('click',function (){forwardClick(image_best_movies, carousel_best_movies_position1,
+carousel_best_movies_position2, carousel_best_movies_position3, carousel_best_movies_position4);});
+click_back_best_movies.addEventListener('click',function (){backClick(image_best_movies, carousel_best_movies_position1,
+carousel_best_movies_position2, carousel_best_movies_position3, carousel_best_movies_position4);});
+
+click_forward_familly.addEventListener('click',function (){forwardClick(list_img_familly, carousel_familly_position1,
+carousel_familly_position2, carousel_familly_position3, carousel_familly_position4);});
+click_back_familly.addEventListener('click',function (){backClick(list_img_familly, carousel_familly_position1,
+carousel_familly_position2, carousel_familly_position3, carousel_familly_position4);});
+
+document.addEventListener ("DOMContentLoaded", function (){ajax_best_movies();});
+document.addEventListener ("DOMContentLoaded", function (){ajax_categorie_familly();});
 
 
-image.addEventListener('click', function() {
-          $dialog.showModal();
-});
